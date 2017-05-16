6b3f0f2f448426ca69dc3298e76a586f032ece63
Jenna <jmschabdach@gmail.com>
2017-05-16 11:16:31 -0400

    added utils directory

---
+++ utils/similarity.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Quick script to calculate correlation ratio
+# (similarity metric for registration)
+# Usage:
+# bash similarity.sh <File1> <File2>
+#output is correlation ratio and mutual information
+
+img1=$1
+img2=$2
+
+echo "# 1mm scale
+setscale 1
+setoption smoothing 1
+setoption boundguess 1
+clear U
+setrow UA  1 0 0 0  0 1 0 0  0 0 1 0  0 0 0 1
+measurecost 7 UA 0 0 0 0 0 0 abs
+printparams U" > sched.txt
+
+flirt -in $img1 -ref $img2 -schedule sched.txt | head -1 | cut -f1 -d' '
+flirt -in $img1 -ref $img2 -cost mutualinfo -schedule sched.txt | head -1 | cut -f1 -d' '
+
+rm sched.txt
---
+++ utils/splitTimepoints.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import argparse
+from nipy import load_image, save_image
+from nipy.core.api import Image
+import os
+
+# set up the argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--image', help='Input image to be split. Must be a 3D time series (4 dimensions)', 
+                    required=True)
+parser.add_argument('-d', '--directory', help='Directory to write the split image to; default = ./tmp/',
+                    default='./tmp/')
+
+# parse the args
+args = parser.parse_args()
+imgFn = args.image
+outDir = args.directory
+
+# check the image for correct dimensionality
+img = load_image(imgFn)
+if len(img.get_data().shape) < 4 or img.get_data().shape[3] == 1:
+    raise TypeError('Error: the image does not have the correct dimensionality')
+
+# check the directory
+if not os.path.exists(outDir):
+    os.mkdir(outDir)
+if not outDir[-1] == '/':
+    # print(outDir)
+    outDir += '/'
+    # print(outDir)
+
+# get base of image filename for use in output files
+outImgBase = imgFn.split('/')[-1].split('.')[0]
+# print('Base name for the output images:', outImgBase)
+
+# split the image
+coord = img.coordmap
+for i in xrange(img.get_data().shape[3]):
+    tmp = img[:,:,:,i].get_data()[:,:,:,None]
+    tmpImg = Image(tmp, coord)
+    outFn = outDir+outImgBase+'_'+str(i).zfill(3)+'.nii.gz'
+    save_image(tmpImg, outFn)
+
+# finished splitting the image
+print('The image', imgFn,'was split into its timepoints.')
+print('The timepoints can be found in:', outDir)
