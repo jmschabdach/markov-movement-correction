86472895e8410191c93f08422e3e1cb849e32b62
Jenna <jmschabdach@gmail.com>
2017-05-19 09:50:07 -0400

    added notebook for figure generation

--- calculateSimilarity.sh
+++ calculateSimilarity.sh
@@ -6,20 +6,30 @@
 #TEMPLATE='/home/jenna/Research/CHP-PIRC/markov-movement-correction/tmp/timepoints/0003_MR1_18991230_000000EP2DBOLDLINCONNECTIVITYs004a001_000.nii.gz'
 TEMPLATE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints/000.nii.gz'
 
-DIR='home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints'
+DIR='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints/*'
 # for all the images in the non-registered version
-for img in "$DIR"/* ; do
-    echo $img
+count=0
+echo "Timepoint, Similarity, Mutual_Information" > /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_preregistration.csv
+for img in $DIR ; do
+    # compare each image to the template
+    sims=$(./utils/similarity.sh $TEMPLATE $img)
+    echo $count, $sims >> /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_preregistration.csv
+    count=$((count+1))
 done
 
-DIR='home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/noAffine'
+DIR='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/noAffine/*'
 # for all the images in the registered (non-affine) version
-for img in "$DIR"/* ; do
-    echo $img
+count=0
+echo "Timepoint, Similarity, Mutual_Information" > /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_registrated_nonAffine.csv
+for img in $DIR ; do
+     # compare each image to the template
+    sims=$(./utils/similarity.sh $TEMPLATE $img)
+    echo $count, $sims >> /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_registrated_nonAffine.csv
+    count=$((count+1))
 done
 
-DIR='home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/registered'
+DIR='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/registered/*'
 # for all the iamges in the registered (yes affine) version
-for img in "$DIR"/* ; do
+for img in $DIR ; do
     echo $img
 done
