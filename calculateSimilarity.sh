d49ce799c629fad8b75f73d40eba8c88a8760044
Jenna <jmschabdach@gmail.com>
2017-05-23 16:12:10 -0400

    still trying to generalize calculateSimilarity

--- calculateSimilarity.sh
+++ calculateSimilarity.sh
@@ -5,41 +5,40 @@
 
 #TEMPLATE='/home/jenna/Research/CHP-PIRC/markov-movement-correction/tmp/timepoints/0003_MR1_18991230_000000EP2DBOLDLINCONNECTIVITYs004a001_000.nii.gz'
 TEMPLATE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints/000.nii.gz'
-
 BASE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp'
 
 DIR="$BASE/timepoints_0/*"
 # for all the images in the non-registered version
 count=0
-echo "Timepoint, Similarity, Mutual_Information" > /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_preregistration.csv
+echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_preregistration.csv"
 for img in $DIR ; do
     # compare each image to the template
     sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_preregistration.csv
+    echo $count, $sims >> "$BASE/similarities_preregistration.csv"
     count=$((count+1))
 done
 
-cp $TEMPLATE /home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/noAffine/ 
+cp $TEMPLATE "$BASE/noAffine/"
 DIR="$BASE/noAffine/*"
 # for all the images in the registered (non-affine) version
 count=0
-echo "Timepoint, Similarity, Mutual_Information" > /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_registered.csv
+echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_registered.csv"
 for img in $DIR ; do
      # compare each image to the template
     sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_registered.csv
+    echo $count, $sims >> "$BASE/similarities_registered.csv"
     count=$((count+1))
 done
 
-cp $TEMPLATE /home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/markov/ 
+cp $TEMPLATE "$BASE/markov/ "
 DIR="$BASE/markov/*"
 # for all the images in the non-registered version
 count=0
-echo "Timepoint, Similarity, Mutual_Information" > /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_markov.csv
+echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_markov.csv"
 for img in $DIR ; do
     # compare each image to the template
     sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> /home/pirc/Desktop/Jenna_dev/markov-movement-correction/similarities_markov.csv
+    echo $count, $sims >> "$BASE/similarities_markov.csv"
     count=$((count+1))
 done
 
