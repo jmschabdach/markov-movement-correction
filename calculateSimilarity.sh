a992405038f76acea285d02f8ef4f84064cfb9f1
Jenna <jmschabdach@gmail.com>
2017-05-23 16:30:31 -0400

    still trying to generalize calculateSimilarity

--- calculateSimilarity.sh
+++ calculateSimilarity.sh
@@ -5,7 +5,8 @@
 
 #TEMPLATE='/home/jenna/Research/CHP-PIRC/markov-movement-correction/tmp/timepoints/0003_MR1_18991230_000000EP2DBOLDLINCONNECTIVITYs004a001_000.nii.gz'
 TEMPLATE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints/000.nii.gz'
-BASE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp'
+# BASE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp'
+BASE=$1
 
 DIR="$BASE/timepoints_0/*"
 # for all the images in the non-registered version
