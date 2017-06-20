3a9cc5db3cc8480155ae66b13a461303ab924679
Jenna <jmschabdach@gmail.com>
2017-06-20 09:21:25 -0400

    updated similarity calculation script

--- calculateSimilarity.sh
+++ calculateSimilarity.sh
@@ -4,11 +4,13 @@
 # metric for 2 images
 
 #TEMPLATE='/home/jenna/Research/CHP-PIRC/markov-movement-correction/tmp/timepoints/0003_MR1_18991230_000000EP2DBOLDLINCONNECTIVITYs004a001_000.nii.gz'
-TEMPLATE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints/000.nii.gz'
+# TEMPLATE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp/timepoints/000.nii.gz'
+# TEMPLATE='/home/pirc/processing/FETAL_Axial_BOLD_Motion_Processing/markov-movement-correction/0003_MR2_18991230_000000EP2DBOLDLINCONNECTIVITYs004a001/timepoints/075.nii.gz'
 # BASE='/home/pirc/Desktop/Jenna_dev/markov-movement-correction/tmp'
 BASE=$1
+TEMPLATE=$2
 
-DIR="$BASE/timepoints_0/*"
+DIR="$BASE/timepoints/*"
 # for all the images in the non-registered version
 count=0
 echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_preregistration.csv"
@@ -19,17 +21,17 @@ for img in $DIR ; do
     count=$((count+1))
 done
 
-# cp $TEMPLATE "$BASE/noAffine/"
-DIR="$BASE/noAffine/*"
-# for all the images in the registered (non-affine) version
-count=0
-echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_registered.csv"
-for img in $DIR ; do
-     # compare each image to the template
-    sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> "$BASE/similarities_registered.csv"
-    count=$((count+1))
-done
+# # cp $TEMPLATE "$BASE/noAffine/"
+# DIR="$BASE/noAffine/*"
+# # for all the images in the registered (non-affine) version
+# count=0
+# echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_registered.csv"
+# for img in $DIR ; do
+#      # compare each image to the template
+#     sims=$(./utils/similarity.sh $TEMPLATE $img)
+#     echo $count, $sims >> "$BASE/similarities_registered.csv"
+#     count=$((count+1))
+# done
 
 # cp $TEMPLATE "$BASE/markov/"
 DIR="$BASE/markov/*"
@@ -43,8 +45,8 @@ for img in $DIR ; do
     count=$((count+1))
 done
 
-DIR="$BASE/registered/*"
-# for all the iamges in the registered (yes affine) version
-for img in $DIR ; do
-    echo $img
-done
+# DIR="$BASE/registered/*"
+# # for all the iamges in the registered (yes affine) version
+# for img in $DIR ; do
+#     echo $img
+# done
