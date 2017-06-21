39fc01a8cd7ce23a8a9adb7f5462e94189483770
Jenna <jmschabdach@gmail.com>
2017-06-21 08:42:01 -0400

    cleaned up similarity calc code, going to attempt an overhaul of it

--- calculateSimilarity.sh
+++ calculateSimilarity.sh
@@ -21,19 +21,6 @@ for img in $DIR ; do
     count=$((count+1))
 done
 
-# # cp $TEMPLATE "$BASE/noAffine/"
-# DIR="$BASE/noAffine/*"
-# # for all the images in the registered (non-affine) version
-# count=0
-# echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_registered.csv"
-# for img in $DIR ; do
-#      # compare each image to the template
-#     sims=$(./utils/similarity.sh $TEMPLATE $img)
-#     echo $count, $sims >> "$BASE/similarities_registered.csv"
-#     count=$((count+1))
-# done
-
-# cp $TEMPLATE "$BASE/markov/"
 DIR="$BASE/markov/*"
 # for all the images in the non-registered version
 count=0
@@ -45,8 +32,13 @@ for img in $DIR ; do
     count=$((count+1))
 done
 
-# DIR="$BASE/registered/*"
-# # for all the iamges in the registered (yes affine) version
-# for img in $DIR ; do
-#     echo $img
-# done
+DIR="$BASE/bifur-markov/*"
+# for all the images in the non-registered version
+count=0
+echo "Timepoint, Similarity, Mutual_Information" > "$BASE/similarities_bifur_markov.csv"
+for img in $DIR ; do
+    # compare each image to the template
+    sims=$(./utils/similarity.sh $TEMPLATE $img)
+    echo $count, $sims >> "$BASE/similarities_bifur_markov.csv"
+    count=$((count+1))
+done
