374e1aa792687b3188a3a8ef86d00995baa9f064
Jenna <jmschabdach@gmail.com>
2017-07-10 11:46:14 -0400

    something funky with transforms

--- hmmMovementCorrection.py
+++ hmmMovementCorrection.py
@@ -795,7 +795,7 @@ def main(baseDir):
         for i in xrange(len(linkingTransFns)):
             alignedFns.extend(hmmCompartments[i])
             # alignCompartments(compartments[i][-1], alignedFns, compartmentTransformFns[i])
-            alignCompartments(compartments[i+1][0], alignedFns, linkingTransFns[i])
+            # alignCompartments(compartments[i+1][0], alignedFns, linkingTransFns[i])
         # alignCompartments(alignedFns[0], alignedFns, linkingTransFns[-1])
         # for i in xrange(len(linkingTransFns)-1, 0, -1):
         #     alignedFns = hmmCompartments[i] + alignedFns
