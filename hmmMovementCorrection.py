ecd93cd9c7225ca596e76ab2b97203df4e7225a7
Jenna <jmschabdach@gmail.com>
2017-07-12 10:23:17 -0400

    switching back to affine for the linking transform

--- hmmMovementCorrection.py
+++ hmmMovementCorrection.py
@@ -279,41 +279,41 @@ def calculateLinkingTransform(prevCompImg, nextCompImg, transformFn):
     reg.inputs.output_transform_prefix = transformFn
     reg.inputs.interpolation = 'NearestNeighbor'
 
-    # # Affine transform
-    # reg.inputs.transforms = ['Affine']
-    # reg.inputs.transform_parameters = [(2.0,)]
-    # reg.inputs.number_of_iterations = [[1500, 200]]
-    # reg.inputs.dimension = 3
-    # reg.inputs.write_composite_transform = True
-    # reg.inputs.collapse_output_transforms = False
-    # reg.inputs.initialize_transforms_per_stage = False
-    # reg.inputs.metric = ['MI']
-    # reg.inputs.metric_weight = [1]
-    # reg.inputs.radius_or_number_of_bins = [32]
-    # reg.inputs.sampling_strategy = ['Random']
-    # reg.inputs.sampling_percentage = [0.05]
-    # reg.inputs.convergence_threshold = [1.e-8]
-    # reg.inputs.convergence_window_size = [20]
-    # reg.inputs.smoothing_sigmas = [[2,1]]
-    # reg.inputs.sigma_units = ['vox']
-    # reg.inputs.shrink_factors = [[2,1]]
-
-    # Nonlinear transform
-    reg.inputs.transforms = ['SyN']
-    reg.inputs.transform_parameters = [(0.25, 3.0, 0.0)]
-    reg.inputs.number_of_iterations = [[100, 50, 30]]
+    # Affine transform
+    reg.inputs.transforms = ['Affine']
+    reg.inputs.transform_parameters = [(2.0,)]
+    reg.inputs.number_of_iterations = [[1500, 200]]
     reg.inputs.dimension = 3
     reg.inputs.write_composite_transform = True
     reg.inputs.collapse_output_transforms = False
     reg.inputs.initialize_transforms_per_stage = False
-    reg.inputs.metric = ['CC']
+    reg.inputs.metric = ['MI']
     reg.inputs.metric_weight = [1]
     reg.inputs.radius_or_number_of_bins = [32]
+    reg.inputs.sampling_strategy = ['Random']
+    reg.inputs.sampling_percentage = [0.05]
     reg.inputs.convergence_threshold = [1.e-8]
     reg.inputs.convergence_window_size = [20]
-    reg.inputs.smoothing_sigmas = [[2,1,0]]
+    reg.inputs.smoothing_sigmas = [[2,1]]
     reg.inputs.sigma_units = ['vox']
-    reg.inputs.shrink_factors = [[3,2,1]]
+    reg.inputs.shrink_factors = [[2,1]]
+
+    # # Nonlinear transform
+    # reg.inputs.transforms = ['SyN']
+    # reg.inputs.transform_parameters = [(0.25, 3.0, 0.0)]
+    # reg.inputs.number_of_iterations = [[100, 50, 30]]
+    # reg.inputs.dimension = 3
+    # reg.inputs.write_composite_transform = True
+    # reg.inputs.collapse_output_transforms = False
+    # reg.inputs.initialize_transforms_per_stage = False
+    # reg.inputs.metric = ['CC']
+    # reg.inputs.metric_weight = [1]
+    # reg.inputs.radius_or_number_of_bins = [32]
+    # reg.inputs.convergence_threshold = [1.e-8]
+    # reg.inputs.convergence_window_size = [20]
+    # reg.inputs.smoothing_sigmas = [[2,1,0]]
+    # reg.inputs.sigma_units = ['vox']
+    # reg.inputs.shrink_factors = [[3,2,1]]
 
     reg.inputs.use_estimate_learning_rate_once = [True]
     reg.inputs.use_histogram_matching = [True] # This is the ult
@@ -897,8 +897,8 @@ def main(baseDir):
 if __name__ == "__main__":
     # set the base directory
     # baseDir = '/home/pirc/processing/FETAL_Axial_BOLD_Motion_Processing/markov-movement-correction/'
-    baseDir = '/home/jms565/Research/CHP-PIRC/markov-movement-correction/'
-    # baseDir = '/home/jenna/Research/CHP-PIRC/markov-movement-correction/'
+    # baseDir = '/home/jms565/Research/CHP-PIRC/markov-movement-correction/'
+    baseDir = '/home/jenna/Research/CHP-PIRC/markov-movement-correction/'
 
     # very crude numpy version check
     npVer = np.__version__
