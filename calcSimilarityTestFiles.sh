6b829c189aa1b2d5028547f6b2f7f9d1225a31ad
Jenna <jmschabdach@gmail.com>
2017-08-07 10:49:22 -0400

    getting a weird flirt error, trying something

--- calcSimilarityTestFiles.sh
+++ calcSimilarityTestFiles.sh
@@ -8,39 +8,41 @@ TEMPLATE="$BASE/timepoints/000.nii.gz"
 
 echo $BASE
 
-DIR="$BASE/timepoints/*"
-# for all the images in the non-registered version
-count=0
-echo "Timepoint, Similarity, Mutual_Information" > "$BASE/testing_similarities_preregistration.csv"
-for img in $DIR ; do
-    # make sure $img != 'template.nii.gz'
-    # compare each image to the template
-    sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> "$BASE/testing_similarities_preregistration.csv"
-    count=$((count+1))
-done
+echo $TEMPLATE
 
-DIR="$BASE/testing/*"
+#DIR="$BASE/timepoints/*"
 # for all the images in the non-registered version
-count=0
-echo "Timepoint, Similarity, Mutual_Information" > "$BASE/testing_similarities_1.csv"
-for img in $DIR ; do
+#count=0
+#echo "Timepoint, Similarity, Mutual_Information" > "$BASE/testing_similarities_preregistration.csv"
+#for img in $DIR ; do
     # make sure $img != 'template.nii.gz'
     # compare each image to the template
-    sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> "$BASE/testing_similarities_1.csv"
-    count=$((count+1))
-done
+#    sims=$(./utils/similarity.sh $TEMPLATE $img)
+#    echo $count, $sims >> "$BASE/testing_similarities_preregistration.csv"
+#    count=$((count+1))
+#done
 
-DIR="$BASE/testing-08012017/*"
+#DIR="$BASE/testing/*"
 # for all the images in the non-registered version
-count=0
-echo "Timepoint, Similarity, Mutual_Information" > "$BASE/testing_similarities_0.csv"
-for img in $DIR ; do
+#count=0
+#echo "Timepoint, Similarity, Mutual_Information" > "$BASE/testing_similarities_1.csv"
+#for img in $DIR ; do
     # make sure $img != 'template.nii.gz'
     # compare each image to the template
-    sims=$(./utils/similarity.sh $TEMPLATE $img)
-    echo $count, $sims >> "$BASE/testing_similarities_0.csv"
-    count=$((count+1))
-done
+#    sims=$(./utils/similarity.sh $TEMPLATE $img)
+#    echo $count, $sims >> "$BASE/testing_similarities_1.csv"
+#    count=$((count+1))
+#done
+
+#DIR="$BASE/testing-08012017/*"
+# for all the images in the non-registered version
+#count=0
+##echo "Timepoint, Similarity, Mutual_Information" > "$BASE/testing_similarities_0.csv"
+#for img in $DIR ; do
+#    # make sure $img != 'template.nii.gz'
+    # compare each image to the template
+#    sims=$(./utils/similarity.sh $TEMPLATE $img)
+#    echo $count, $sims >> "$BASE/testing_similarities_0.csv"
+#    count=$((count+1))
+#done
 
