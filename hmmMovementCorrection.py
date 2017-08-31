bd15b8d25f19077bd1f7682737e261e8beaa5638
Jenna <jmschabdach@gmail.com>
2017-08-31 10:15:11 -0400

    something is not right

--- hmmMovementCorrection.py
+++ hmmMovementCorrection.py
@@ -421,7 +421,7 @@ def registerToTemplate(fixedImgFn, movingImgFn, outFn, outDir, transformPrefix, 
         reg.inputs.initial_moving_transform = transformPrefix+'0Warp.nii.gz'
         reg.inputs.invert_initial_moving_transform = False
 
-    # print(reg.cmdline)
+    print(reg.cmdline)
     print("Starting nonlinear registration for",outFn)
     reg.run()
     print("Finished running nonlinear registration for", outFn)
