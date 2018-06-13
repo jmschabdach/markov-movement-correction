c7138706bc9636b54ac943a6077e99b372d05c57
Jenna <jmschabdach@gmail.com>
2018-06-12 22:47:39 -0400

    updating runAll.sh for beginning of the second round of analysis

--- runAll.sh
+++ runAll.sh
@@ -3,7 +3,8 @@
 # TODO: generalize the filename out
 
 # set up any vars here
-BASE='/home/jms565/Research/CHP-PIRC/markov-movement-correction/Controls/'
+# BASE='/home/jms565/Research/CHP-PIRC/markov-movement-correction/Controls/'
+BASE='/home/jms565/Research/CHP-PIRC/markov-movement-correction/analysisII/Pittsburgh/'
 # BASE='/home/jenna/Research/CHP-PIRC/sandbox/'
 # BASE='/home/pirc/processing/Motion_Correction/Controls/'
 # TEMPLATE="$BASE/timepoints/000.nii.gz"
