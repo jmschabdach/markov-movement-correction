6baa3558801b0597f22b3e296f888e8966c57029
Jenna Schabdach <jmschabdach@gmail.com>
2018-11-14 09:47:46 -0500

    Added functionality to send stdout and stderr to email

--- runAndNotify.sh
+++ runAndNotify.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-"$@"
+"$@" |& tee command_output.txt
 
 LOC="DBMI"
 
 MESSAGE=$'\nYour code has finished running on'
 MESSAGE="$MESSAGE $LOC. You should go check on the results."
 
-echo "$@ $MESSAGE" | mail -s "Code Completion Notification" jmschabdach@gmail.com
+(echo "$@ $MESSAGE"; cat command_output.txt) > mail_notification.txt
+
+mail -s "Code Completion Notification" jmschabdach@gmail.com < mail_notification.txt
+
+rm mail_notification.txt
+rm command_output.txt
