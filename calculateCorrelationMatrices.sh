020518d4ad2aad2ccb0b73be4beab68e27fa3a7e
Jenna Schabdach <jmschabdach@gmail.com>
2018-11-14 10:09:12 -0500

    updated correlation matrix calculations with pid based names for temp files and something else

---
+++ calculateCorrelationMatrices.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Calculate the correlation matrices for the stacking-hmm test results
+
+# bash calculateCorrelationMatrix.sh 0003_MR1_18991230_000000EP2DBOLDLINCONNECTIVITYs004a001/stacking-hmm/ metrics/correlationMatrix-stackingHmm-5c.csv
+
+#BASE='/home/jenna/Research/CHP-PIRC/markov-movement-correction/Controls'
+BASE=$1
+
+for i in "$BASE"/* ; do
+    if [ -d "$i" ] ; then
+        # set up the directories to reference
+        mkdir "$i/metrics/"
+        HMM="$i/hmm/"
+        FIRST="$i/firstTimepointMatching/"
+        STACK="$i/stacking-hmm/"
+        ORIG="$i/timepoints/"
+        # set up the .csv files to write to
+        FIRST_FN="$i/metrics/crossCorrelation-firstTimepoint.csv"
+        HMM_FN="$i/metrics/crossCorrelation-hmm.csv"
+        STACK_FN="$i/metrics/crossCorrelation-stackingHmm.csv"
+        ORIG_FN="$i/metrics/crossCorrelation-original.csv"
+        # calculate the correlation ratio matrices
+	bash calculateCorrelationMatrix.sh $ORIG $ORIG_FN
+        # bash calculateCorrelationMatrix.sh $FIRST $FIRST_FN
+        # bash calculateCorrelationMatrix.sh $HMM $HMM_FN
+        # bash calculateCorrelationMatrix.sh $STACK $STACK_FN
+    fi
+done
+
+#bash calculateCorrelationMatrix.sh "/home/jenna/Research/CHP-PIRC/markov-movement-correction/LinearControls/0784_TC_015/firstTimepointMatching/" "/home/jenna/Research/CHP-PIRC/markov-movement-correction/LinearControls/0784_TC_015/metrics/crossCorrelation-firstTimepoint.csv"
+
+#bash calculateCorrelationMatrix.sh "/home/jenna/Research/CHP-PIRC/markov-movement-correction/LinearControls/0794_TC_026/firstTimepointMatching/" "/home/jenna/Research/CHP-PIRC/markov-movement-correction/LinearControls/0794_TC_026/metrics/crossCorrelation-firstTimepoint.csv"
+
+#bash calculateCorrelationMatrix.sh "/home/jenna/Research/CHP-PIRC/markov-movement-correction/LinearControls/0799_TC_031/firstTimepointMatching/" "/home/jenna/Research/CHP-PIRC/markov-movement-correction/LinearControls/0799_TC_031/metrics/crossCorrelation-firstTimepoint.csv"
---
+++ correlation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Quick script to calculate correlation ratio
+# (similarity metric for registration)
+# Usage:
+# bash similarity.sh <File1> <File2>
+#output is correlation ratio and mutual information
+
+img1=$1
+img2=$2
+
+sched="/tmp/schedule_"$$".txt"
+
+echo "# 1mm scale
+setscale 1
+setoption smoothing 1
+setoption boundguess 1
+clear U
+setrow UA  1 0 0 0  0 1 0 0  0 0 1 0  0 0 0 1
+measurecost 7 UA 0 0 0 0 0 0 abs
+printparams U" > $sched
+
+flirt -in $img1 -ref $img2 -schedule $sched | head -1 | cut -f1 -d' '
+
+rm $sched
