485bde56be5397859cc11a58809a5ff63f0040bb
Jenna <jmschabdach@gmail.com>
2019-12-04 13:52:39 -0500

    added file checking

--- meta_compareImgAndDir.py
+++ meta_compareImgAndDir.py
@@ -1,5 +1,6 @@
 import argparse
 from boldli import ImageManipulatingLibrary as mil
+import os
 import sys
 
 def main():
@@ -10,10 +11,15 @@ def main():
 
     # Parse the arguments
     args = parser.parse_args()
-    print(args)
-
     seqFn = args.file
     seqPath = args.directory
+
+    # Check to make sure the image and the directory exist
+    if not os.path.exists(seqFn):
+        raise IOError("Error: the specified image does not exist: "+ seqFn)
+
+    if not os.path.exists(seqPath):
+        raise IOError("Error: the specified directory does not exist: "+ seqPath)
 
     # Load the image from the file
     seqImg, coords = mil.loadBOLD(seqFn)
@@ -24,8 +30,10 @@ def main():
     status = mil.compareImages(seqImg, seqDir)
 
     if status:
+        print("Images are equal")
         sys.exit(0)
     else:
+        print("Images are different")
         sys.exit(1)
 
 if __name__ == "__main__":
