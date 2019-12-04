babc25461c14295b92112947870ea27b93e8c3d4
Jenna <jmschabdach@gmail.com>
2019-12-04 13:44:40 -0500

    expanded for whole site

--- meta_removeRedundantDirectories.sh
+++ meta_removeRedundantDirectories.sh
@@ -1,15 +1,48 @@
 #!/usr/bin/bash
 
-IMG=$1
-DIR=$2
+TYPE=$1
+SITE=$2
 
-python meta_compareImgAndDir.py -d $DIR -f $IMG
+for subj in $SITE/*/ ; do
 
-# Get the exit status of the script
-status=$?
+    if [[ $TYPE == "original" ]] ; then
+        echo "original"
+        IMG="$subj/BOLD.nii"
+        if [ -f "$IMG.gz" ] ; then
+            IMG="$IMG.gz"
+        fi
 
-if [ $status -eq 0 ] ; then
-    echo "Redundant directory detected"
-else
-    echo "OOPS: Sequence and directory contain different info"
-fi
+        DIR=""
+        if [ -d "$subj/original" ] ; then
+            DIR="$subj/original"
+        elif [ -d "$subj/timepoints" ] ; then
+            DIR="$subj/timepoints"
+        fi
+
+    elif [[ $TYPE == "traditional" ]] ; then
+        echo "traditional"
+        IMG="$subj/corrected_traditional.nii.gz"
+        DIR="$subj/traditional"
+
+    elif [[ $TYPE == "dag" ]] ; then
+        echo "dag"
+        IMG="$subj/corrected_dag.nii.gz"
+        DIR="$subj/dag"
+
+    else
+        echo "Type not recognized"
+        exit -1
+    fi
+
+    python meta_compareImgAndDir.py -d $DIR -f $IMG
+
+    # Get the exit status of the script
+    status=$?
+    
+    if [ $status -eq 0 ] ; then
+        echo "Redundant directory detected, removing it now"
+        #rm -rf $DIR
+    else
+        echo "OOPS: Sequence and directory contain different info or an error occurred"
+    fi
+done
